8a45946e0dd6b18428c4086cf84d9e54ebae2d1e
Cristo Rodríguez <crodlar218@g.educaand.es>
2022-11-17 12:59:35 +0100

    Added default null on name in sql in source

--- db/database.sql
+++ db/database.sql
@@ -4,7 +4,7 @@ USE companydb;
 
 CREATE TABLE employee (
     id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45),
+    name VARCHAR(45) DEFAULT NULL,
     salary INT(5) DEFAULT NULL,
     PRIMARY KEY (id)
 );
